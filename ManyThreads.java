d27eae4bc5c425e8a98f751b1edf489f82ee1208
scruf <ek5442@cs.rit.edu>
2015-11-16 20:27:01 -0500

    ManyThreads class added Checking for errors is comming

--- ManyThreads.java
+++ ManyThreads.java
@@ -1,18 +1,24 @@
+import java.util.ArrayList;
+public class ManyThreads {
 
-public class ManyThreads implements Runnable{
-
-
-
-  public static void main(String []args){
-    ManyThreads many = new ManyThreads();
-    Thread thread = new Thread(many);
-
-      thread.start();
-    
-  }
-  @Override
-  public void run(){
-  for(int i=0;i<10;i++)
-    System.out.println("Hello world from a thread "+Thread.currentThread().getId());
-  }
+  public static int size=0;
+ public static ArrayList<Thread> threadList = new ArrayList<>(5);
+ public void test()throws InterruptedException{
+   while(size<5){
+     synchronized(threadList){
+       threadList.add(new Thread(){
+         @Override public void run(){
+           System.out.println("Hello world from "+Thread.currentThread().getId());
+         }
+       });
+     }
+     size++;
+   }
+   for(Thread t : threadList){
+     t.start();
+   }
+ }
+  public static void main(String []args)throws InterruptedException{
+    new ManyThreads().test();
+}
 }
