c2418bba2b697d9fee8a16409868fe3b4928ad34
Egor <ekozitski@gmail.com>
2015-11-17 16:36:19 +0000

    ManyThread program is finnished

--- ManyThreads.java
+++ ManyThreads.java
@@ -3,8 +3,10 @@ public class ManyThreads {
 
   public static int size=0;
  public static ArrayList<Thread> threadList = new ArrayList<>(5);
- public void test()throws InterruptedException{
-   while(size<5){
+ 
+ public void test(int num)throws InterruptedException{
+    
+   while(size<num){
      synchronized(threadList){
        threadList.add(new Thread(){
          @Override public void run(){
@@ -19,6 +21,22 @@ public class ManyThreads {
    }
  }
   public static void main(String []args)throws InterruptedException{
-    new ManyThreads().test();
+    int number=0;
+      if(args.length>1){
+        System.out.println("ManyThreads:  Invalid number");
+        System.exit(1);
+      }
+      if (args.length<1){
+        System.out.println("Usage:  java ManyThreads number-of-threads");
+          System.exit(1);
+      }
+    try{
+        number = Integer.parseInt(args[0]);
+    }catch(Exception ex){
+        System.out.println("Usage: java ManyThread you entered not Integer");
+        System.exit(1);
+    }
+    number = Integer.parseInt(args[0]);
+    new ManyThreads().test(number);
 }
 }
