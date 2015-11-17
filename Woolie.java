f10d0ef493155a210e199ede93e3db904f982583
Egor <ekozitski@gmail.com>
2015-11-17 19:14:41 +0000

    Partial implementation of Test cases is implemented Wollie class is created

--- Woolie.java
+++ Woolie.java
@@ -24,10 +24,30 @@ public class Woolie extends java.lang.Thread implements Runnable{
     public java.lang.String getDestination(){return this.destination;}
     public void run(){
         System.out.println(getWoolieName()+" has arrived at the bridge");
-        System.out.print(getWoolieName()+" is starting to cross");
-        
+        System.out.println(getWoolieName()+" is starting to cross");
+        int time = 0;
+        while(time<getCrossingTime()-1){
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException ie){
+                System.out.print(ie);
+            }
+            System.out.println("\t "+getWoolieName()+" "+(time+1)+" seconds");
+            time++;
+        }
+        System.out.println(getWoolieName()+" leaves at "+getDestination());
+      
     }
     public static void main(String []args){
-        new Woolie("Bob",1,"Mordor").start();
+       Thread Woolie1 = new Thread(new Woolie("Frodo",5,"Mordor"));
+       Thread Wollie2 = new Thread(new Woolie("Gandalf",3,"Minas Tirith"));
+       try{
+           Woolie1.start();
+           Wollie2.start();
+           Woolie1.join();
+           Wollie2.join();
+       }catch(InterruptedException ie){
+           
+       }
     }
 }
