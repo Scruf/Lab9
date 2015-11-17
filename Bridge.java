85d1c4edf3dc0d3026685be5cc45a856dc2ecc86
Egor <ekozitski@hotmail.com>
2015-11-17 19:41:43 +0000

    Initial Bridge commit

--- Bridge.java
+++ Bridge.java
@@ -1,4 +1,7 @@
+import java.util.Queu;
+
 public class Bridge extends java.lang.Object implements Runnable{
+    
     public Bridge(){}
     public void enterBridge(){}
     public void leaveBridge(){}
